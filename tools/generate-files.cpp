99c3bab52735d4ad534e2cac95dd92898eff1b59
Florian Walch <florian@fwalch.com>
2013-06-06 12:43:32 +0200

    Fixed generate-files tool

--- tools/generate-files.cpp
+++ tools/generate-files.cpp
@@ -22,6 +22,10 @@ inline int usageMessage(const char* argv0) {
   return 1;
 }
 
+inline bool skipLine(std::string line) {
+  return line.empty() || line[0] == '#';
+}
+
 int main(int argc, const char** argv) {
   if (argc != 3) {
     return usageMessage(argv[0]);
@@ -71,37 +75,28 @@ int main(int argc, const char** argv) {
   std::cout << " > " << largeFileName << " (ratio " << 1-ratio << ")" << std::endl;
   std::cout << " > " << smallFileName << " (ratio " << ratio << ")" << std::endl;
 
-  int64_t lineCount = 0;
-  int64_t offset = 0;
+  uint64_t offset = 0;
   std::string line;
+  while (std::getline(file, line).good()
+      && !(boost::starts_with(line, "#@ <id_") || (!line.empty() && line[0] == '<'))) {
+    offset++;
+  }
+
+  uint64_t lineCount = 0;
   while (std::getline(file, line).good()) {
-    if (line.empty()) {
-      continue;
-    }
-    lineCount++;
-    //TODO
-    if (offset == 0 && boost::starts_with(line, "#@ <id_")) {
-      offset = lineCount;
-      lineCount = 1;
+    if (!skipLine(line)) {
+      lineCount++;
     }
   }
 
-  if (lineCount < 0) {
-    std::cerr << "File " << fileName << " is too large." << std::endl;
-    return 2;
-  }
-
-  assert(lineCount%2 == 0);
-  lineCount /= 2;
-
-  int64_t smallFileTuples = static_cast<int64_t>(ratio*lineCount);
-  std::unordered_set<int64_t> lineNumbers(static_cast<size_t>(smallFileTuples));
+  uint64_t smallFileTuples = static_cast<uint64_t>(ratio*lineCount);
+  std::unordered_set<uint64_t> lineNumbers(static_cast<size_t>(smallFileTuples));
 
   std::random_device device;
   std::mt19937_64 engine(device());
-  std::uniform_int_distribution<int64_t> dist(0, lineCount);
+  std::uniform_int_distribution<uint64_t> dist(0, lineCount-1);
 
-  int64_t i = 1;
+  uint64_t i = 1;
   while (i <= smallFileTuples) {
     lineNumbers.insert(dist(engine));
     if (lineNumbers.size() == static_cast<size_t>(i)) {
@@ -116,36 +111,33 @@ int main(int argc, const char** argv) {
   file.clear();
   file.seekg(0, file.beg);
 
-  i = 0;
-  while (i < offset) {
+  for (i = 0; i < offset; i++) {
     assert(std::getline(file, line).good());
     smallFile << line << std::endl;
     largeFile << line << std::endl;
-
-    if (!line.empty()) {
-      i++;
-    }
   }
 
-  i = 1;
-  while (std::getline(file, line).good()) {
-    if (lineNumbers.find(i/2) != lineNumbers.end()) {
+  i = 0;
+  while(std::getline(file, line).good()) {
+    if (skipLine(line)) {
+      continue;
+    }
+
+    if (lineNumbers.find(i) != lineNumbers.end()) {
       smallFile << line << std::endl;
     }
     else {
       largeFile << line << std::endl;
     }
 
-    if (!line.empty()) {
-      i++;
-    }
+    i++;
   }
 
   smallFile.close();
   largeFile.close();
   file.close();
 
-  assert(i == 2*lineCount);
+  assert(i == lineCount);
 
   std::cout << "Done." << std::endl;
 
